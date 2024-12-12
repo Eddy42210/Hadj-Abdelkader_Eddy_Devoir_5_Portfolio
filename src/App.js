87047541a2af3297155c48d38bceb8b69a43eb80
Eddy42210 <eddyhadj0@gmail.com>
2024-12-12 09:44:10 +0100

    creation pages + routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Mentions from './pages/Mentions';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Mentions" element={<Mentions />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const Home = () => {
+    return (
+        <div className="Presentation">
+                <h1>Bonjour je suis John Doe</h1>
+                <h2>Développeur web full stack</h2>
+            </div>
+    )
+};
+
+export default Home;
