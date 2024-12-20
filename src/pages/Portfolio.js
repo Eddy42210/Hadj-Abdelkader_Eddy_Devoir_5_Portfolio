0ae4c12f2be4e24cbcba0f8fb1aba42dffe40079
Eddy42210 <eddyhadj0@gmail.com>
2024-12-20 14:27:59 +0100

    #8 ok

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,99 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
+import './Portfolio.css';
+import FreshFoodImage from "../images/portfolio/fresh-food.jpg";
+import RestaurantImage from "../images/portfolio/restaurant-japonais.jpg";
+import BienEtreImage from "../images/portfolio/espace-bien-etre.jpg";
+import SeoImage from "../images/portfolio/seo.jpg";
+import ApiImage from "../images/portfolio/coder.jpg";
+import MaquetteImage from "../images/portfolio/screens.jpg";
 
+const Button = () => {
+    return (
+        <Link to="/Portfolio" className='button'>Voir le site</Link>
+    );
+};
+
+const CardPortfolio = ({ image, title, text, footerText }) => {
+    return (
+        <div className="card">
+            <img src={image} className="card-img-top" alt={title} />
+            <div className="card-body">
+                <h3 className="card-title">{title}</h3>
+                <p className="card-text">{text}</p>
+            </div>
+            <Button />
+            <div className="card-footer">
+                <small className="text-body-secondary">{footerText}</small>
+            </div>
+        </div>
+    );
+};
 
 const Portfolio = () => {
     return (
-    <div>
-        <h1>Portfolio</h1>
-
-        <p>Voici quelques-unes de mes réalisations</p>
-        
-        </div>
-)};
+        <main>
+            <div className='page'>
+                <h1>Portfolio</h1>
+                <p className='descriptif'>Voici quelques-unes de mes réalisations</p>
+            </div>
+            <div className='articles'>
+                <div className='ensemble'>
+                    <article>
+                        <CardPortfolio 
+                            image={FreshFoodImage}
+                            title="Fresh Food" 
+                            text="Site de vente de produits frais en ligne" 
+                            footerText="Site réalisé avec PHP et MySQL" 
+                        />
+                    </article>
+                    <article>
+                        <CardPortfolio 
+                            image={RestaurantImage} 
+                            title="Restaurant Akira" 
+                            text="Site de vente de produits frais en ligne" 
+                            footerText="Site réalisé avec WordPress" 
+                        />
+                    </article>
+                    <article>
+                        <CardPortfolio 
+                            image={BienEtreImage} 
+                            title="Espace bien-être" 
+                            text="Site de vente de produits frais en ligne" 
+                            footerText="Site réalisé avec LARAVEL" 
+                        />
+                    </article>
+                </div>
+                <div className='ensemble2'>
+                    <article>
+                        <CardPortfolio 
+                            image={SeoImage} 
+                            title="SEO" 
+                            text="Amélioration du référencement d'un site e-commerce" 
+                            footerText="Utilisation des outils SEO" 
+                        />
+                    </article>
+                    <article>
+                        <CardPortfolio 
+                            image={ApiImage} 
+                            title="Création d'une API" 
+                            text="Création d'une API RESTFULL publique" 
+                            footerText="PHP - SYMFONY" 
+                        />
+                    </article>
+                    <article>
+                        <CardPortfolio 
+                            image={MaquetteImage} 
+                            title="Maquette d'un site web" 
+                            text="Création du prototype d'un site" 
+                            footerText="Réalisé avec FIGMA" 
+                        />
+                    </article>
+                </div>
+            </div>
+        </main>
+    );
+};
 
 export default Portfolio;
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Services.css';
 import '../App.css';
-import CardComponent from '../components/Cards';
 
 
 function Services() {
@@ -10,20 +9,53 @@ function Services() {
         <div className='banner'>
         </div>
      
-    <div className='service'>
+    <div className='page'>
             <h1 >Mon offre de services</h1>
-    <p className='offre'>Voici les prestations sur lesquelles je peux intervenir</p>
+    <p className='descriptif'>Voici les prestations sur lesquelles je peux intervenir</p>
     </div>
     <div className='cards'>
-           
-            <CardComponent />
-
-
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-body">
+            <i class="bi bi-brush"></i>
+              <h2 className="card-title">UX Design</h2>
+              <p className="card-text">L'UX Design est une discipline qui consiste à concevoir
+                des produits (sites webs, applications mobiles, logiciels, objets connectés, etc.) en plaçant
+                l'utilisateur au centre des préoccupations. L'objectif est de rendre l'expérience utilisateur la 
+                plus fluide et agréable possible.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-body2">
+            <i class="bi bi-code-slash"></i>
+              <h2 className="card-title">Développement Web</h2>
+              <p className="card-text">Le développement de sites web consiste à créer des sites internets en utilisant
+                des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks 
+                (Bootstrap, React, Angular, etc.).
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mb-4">
+            <div className="card-body3">
+            <i class="bi bi-search"></i>
+              <h2 className="card-title">Référencement</h2>
+              <p className="card-text">Le Référencement naturel (SEO) est une technique qui consiste à optimiser un site web
+                pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, Yahoo, etc.). L'objectif
+                est d'attirer un maximum de visiteurs qualifié sur le site.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
     </div>
-
-
-    
-    
     </main>
 )};
 
